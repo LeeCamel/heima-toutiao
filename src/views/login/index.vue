3c85f8f3e7d48f8bb0107c731b6448ed45f605bd
luotuo <15801609400@163.com>
2021-01-13 23:15:42 +0800

    登录消息提示、loading处理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -19,7 +19,7 @@
           <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
+          <el-button class="login-btn" type="primary" @click="onLogin" :loading="loginLoading">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -27,6 +27,7 @@
 </template>
 
 <script>
+import request from '@/utils/request'
 export default {
   name: 'LoginIndex',
   components: {},
@@ -37,7 +38,8 @@ export default {
         mobile: '', // 手机号
         code: '' // 验证码
       },
-      checked: false // 是否同意协议的选中状态
+      checked: false, // 是否同意协议的选中状态
+      loginLoading: false
     }
   },
   computed: {},
@@ -47,8 +49,37 @@ export default {
   mounted () {
   },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    onLogin () {
+      // 获取表单数据（根据接口要求绑定数据）
+      const user = this.user
+
+      // 表单验证
+
+      // 开启登录中loading
+      this.loginLoading = true
+
+      // 验证通过，提交登录
+      request({
+        method: 'POST',
+        url: '/mp/v1_0/authorizations',
+        // data 用来设置 POST 请求体
+        data: user
+      }).then(res => {
+        // 登录成功
+        console.log(res)
+        this.$message({
+          message: '登录成功',
+          type: 'success'
+        })
+        // 关闭loading
+        this.loginLoading = false
+      }).catch(err => {
+        // 登录失败
+        console.log('登录失败', err)
+        this.$message.error('登录失败，手机号或验证码错误')
+        // 关闭loading
+        this.loginLoading = false
+      })
     }
   }
 }
