26a7e24bf01de4d4254a00dab43d79c1a0ab768a
李欣 <xin.li01@yiducloud.cn>
2021-01-15 01:33:07 +0800

    Layout

--- src/api/user.js
+++ src/api/user.js
@@ -12,3 +12,19 @@ export const login = (data) => {
     data // 等价于data: data
   })
 }
+
+// 获取用户信息
+export const getUserProfile = () => {
+  return request({
+    method: 'GET',
+    url: '/mp/v1_0/user/profile',
+    // 后端要求把需要授权的用户身份放到请求头中
+    // axios 可以通过 headers 选项设置请求头
+    headers: {
+      // 属性名和值都得看接口的要求
+      // 属性名：Authorization，接口要求的
+      // 属性值：Bearer空格token数据
+      Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDIwOTMzNjQsInVzZXJfaWQiOjEsInJlZnJlc2giOmZhbHNlLCJ2ZXJpZmllZCI6dHJ1ZX0.k7xmLPDUHnWGHBaf0hejAiDZep207Xd6tWTW9cnQJHc'
+    }
+  })
+}
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="layout-container">
-    <div>顶部导航栏</div>
-
-    <div>侧边导航栏</div>
-
-    <!-- 子路由出口 -->
-    <router-view />
+    <el-container class="layout-container">
+      <el-aside class="aside" width="200px">
+        <AppAside class="aside-menu"></AppAside>
+      </el-aside>
+      <el-container>
+        <el-header class="header">
+          <Header></Header>
+        </el-header>
+        <el-main class="main">
+          <!-- 子路由出口 -->
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import AppAside from './components/aside'
+import Header from './components/header'
+
 export default {
   name: 'LayoutIndex',
-  components: {},
+  components: {
+    AppAside,
+    Header
+  },
   props: {},
   data () {
     return {}
@@ -25,4 +39,27 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.layout-container {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+
+.aside {
+  background-color: #d3dce6;
+  .aside-menu {
+    height: 100%;
+  }
+}
+
+.header {
+  background-color: #b3c0d1;
+}
+
+.main {
+  background-color: #e9eef3;
+}
+</style>
